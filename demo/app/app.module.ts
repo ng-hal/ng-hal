efb6a6313ddbf0b0076f0ff0368d0295c498abb0
David Herges <david@spektrakel.de>
2017-02-09 08:13:34 +0100

    adds test for HalModule
    ... Cannot find module "./hal.module"

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -13,7 +13,7 @@ import { AppComponent }       from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    HalModule
+    HalModule.forRoot()
   ]
 })
 export class AppModule {}
--- lib/hal.module.ts
+++ lib/hal.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { ConversionStrategy } from './conversion-strategy';
 import { ConversionStrategyJson } from './conversion-strategy-json';
 import { Navigator } from './navigator';
 
+
+/** Angular module for HAL Navigator. */
 @NgModule({
-  providers: [
-    { provide: ConversionStrategy, useClass: ConversionStrategyJson },
-    Navigator
-  ]
 })
 export class HalModule {
+
+
+  /** Return a module for HAL Navigator with root providers. */
+  public static forRoot(): ModuleWithProviders {
+
+    return {
+      ngModule: HalModule,
+      providers: [
+        { provide: ConversionStrategy, useClass: ConversionStrategyJson },
+        { provide: Navigator, useClass: Navigator }
+      ]
+    };
+  }
+
 }
