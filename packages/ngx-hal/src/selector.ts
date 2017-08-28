e81400d452c1f220b5b40229254dba583f0f5864
David Herges <dherges@users.noreply.github.com>
2017-08-28 22:13:18 +0200

    feat: add builder idea

--- packages/ngx-hal/src/selector.ts
+++ packages/ngx-hal/src/selector.ts
@@ -61,3 +61,16 @@ export const embeddedFrom = (res: Resource) => {
  embeddedFrom(cart).rels('item', 2);
  embeddedFrom(customer).rels('orders', 7);
  */
+
+
+// TODO: builders for resources would also be cool :-)
+// https://github.com/dherges/ng-hal/issues/6#issuecomment-325466599
+/*
+this.http.post(linksFrom(foo).rel('bar'), resourceFrom({}).withSelfHref('foo/bar') )
+
+const foo = { _links: { self: { href: 'foo/bar' } } };
+resourceFrom({}).withSelfRel(foo));
+
+resourceFrom({}).withRel('next', 'foo/bar'));
+resourceFrom({}).withRel('next', { href: 'foo/{bar}', templated: true } ));
+*/
