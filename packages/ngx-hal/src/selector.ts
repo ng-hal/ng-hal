64e34c6efe1801f9d66489b5262df2311cd5cbdd
David Herges <dherges@users.noreply.github.com>
2017-08-28 21:59:29 +0200

    feat: draft code for css-like selector traversing links and embedded resources

---
+++ packages/ngx-hal/src/selector.ts
@@ -0,0 +1,63 @@
+// IDEA: a DSL for navigating HAL documents w/ CSS-like selectors
+
+
+export const linksFrom = (res: Resource) => {
+
+  return {
+    rel: (name: string, params?: any): string => {
+      const link = res && res._links ? res._links[rel] : undefined;
+      
+      if (link && link.templated) {
+        return expand(link.href, params);
+      } else if (link) {
+        return link.href;
+      }
+
+      throw new Error(`Unknown relation ${rel} in resource ${res._links.self.href}`);
+    },
+    rels: (name: string, index: number, params?: any): string => {
+      // TODO: good notion?!?
+      // linksFrom(foo).rels('foo', 2, {});
+    }
+  };
+};
+
+/*
+ Usage:
+
+ linksFrom(list).rel('next');
+
+ linksFrom(list).rel('page', { number: 7 });
+
+ linksFrom(index).rel('collection', { offset: 12, limit: 30, sortBy: 'foo', sortOrder: 'DESC' });
+
+*/
+
+
+export const embeddedFrom = (res: Resource) => {
+  
+  return {
+    rel: (rel: string): Resource => {
+      const embeddedResource = res && res._embedded ? res._embedded[rel] : undefined;
+
+      if (embeddedResource) {
+        return embeddedResource;
+      }
+
+      throw new Error(`No embedded resource ${rel} in ${res._links.self.href}`);
+    },
+    rels: (rel: string, index: number): Resource => {
+      // embeddedFrom(foo).rels('foo', 3);
+    }
+  };
+};
+
+/* Usage:
+ 
+ embeddedFrom(foo).rel('list');
+ embeddedFrom(slice).rel('collection');
+ embeddedFrom(foobar).rel('page');
+ embeddedFrom(think).rel('form');
+ embeddedFrom(cart).rels('item', 2);
+ embeddedFrom(customer).rels('orders', 7);
+ */
